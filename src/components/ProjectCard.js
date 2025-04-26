800544a54d47d75625693179790f08e240249ebb
Baobaos <bchang758@gmail.com>
2025-04-26 16:40:17 -0700

    project card component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import "./ProjectCard.css"
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export const ProjectCard = ({ name, imageUrl, description }) => {
     return (
-        <>
-            <Box className="card-box">
-                <h2>{name}</h2>
-                <div>
-                    <img src={imageUrl} alt={name} className="image" />
-                    <p>{description}</p>
-                </div>
-            </Box>
-        </>
+
+        <div className="card-overlay-container">
+            <img src={imageUrl} alt="..." className="card-overlay-image" />
+            <div className="card-overlay-text-right">
+                <h5 className="card-title">Card Title</h5>
+                <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional content.
+                </p>
+                <button className='case-study-button'><p className="button-text">View Case Study</p></button>
+            </div>
+        </div>
+
+
+
     )
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavBar } from '../components/NavBar'
 import { ProjectCard } from '../components/ProjectCard'
+import "./Projects.css"
 
 
 const Projects = () => {
@@ -8,19 +9,18 @@ const Projects = () => {
         <>
             <NavBar/>
             <hr/>
-            <h1>Projects</h1>
-            <p>Here is a collection of projects that I have worked on so far.</p>
+            <h1>UI/UX Projects</h1>
             <div className="project-cards-container">
                 <ProjectCard
                     name="Pantry Pal"
                     imageUrl="https://picsum.photos/seed/picsum/200/300"
                     description="Have you ever found yourself staring at your fridge, unsure of what to cook for dinner? A staggering 62% of Americans have reported feeling stressed about meal preparation. They cite lack of time, inspiration, or ingredients as their main challenges. Recognizing this common struggle, my team, Silent Strivers, and I worked together to design an app that addresses and solves this issue. Our goal was to provide users with a convenient tool for brainstorming meal ideas effortlessly."
                 />
-                <ProjectCard
+                {/* <ProjectCard
                     name="Sonar Mental Health - Improved Onboarding"
                     imageUrl="https://picsum.photos/seed/picsum/200/300"
                     description="Have you ever found yourself staring at your fridge, unsure of what to cook for dinner? A staggering 62% of Americans have reported feeling stressed about meal preparation. They cite lack of time, inspiration, or ingredients as their main challenges. Recognizing this common struggle, my team, Silent Strivers, and I worked together to design an app that addresses and solves this issue. Our goal was to provide users with a convenient tool for brainstorming meal ideas effortlessly."
-                />
+                /> */}
             </div>
         </>
     )
