94ab1ffd35b37bf32cdf87cef1d3319c6114dce2
Baobaos <bchang758@gmail.com>
2025-05-07 01:29:13 -0700

    deleted case study button; entire project card is clickable

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,14 +6,15 @@ export const ProjectCard = ({ name, imageUrl, description, pageLink, ...props })
     return (
 
         <div className="card-overlay-container" {...props}>
-            <img src={imageUrl} alt="..." className="card-overlay-image" />
-            <div className="card-overlay-text-right">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text">
-                    {description}
-                </p>
-                <Link to={pageLink}><button className='case-study-button'><p className="button-text">View Case Study</p></button></Link>
-            </div>
+            <Link to={pageLink}>
+                <img src={imageUrl} alt="..." className="card-overlay-image" />
+                <div className="card-overlay-text-right">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">
+                        {description}
+                    </p>
+                </div>
+            </Link>
         </div>
 
 
